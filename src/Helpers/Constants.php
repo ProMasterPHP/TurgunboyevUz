59993dda0e79ebe65093f5afd1bef9c3fb72723b
ProMasterPHP <67572561+ProMasterPHP@users.noreply.github.com>
2023-03-09 20:19:47 +0500

    Create Constants.php

---
+++ src/Helpers/Constants.php
@@ -0,0 +1,23 @@
+<?php
+define('TG_LIBRARY_VERSION',"1.0");
+
+define('TG_MESSAGE','message');
+define('TG_EDITED_MSG','edited_message');
+define('TG_CHANNEL_POST','channel_post');
+define('TG_EDITED_CHANNEL','edited_channel_post');
+define('TG_INLINE','inline_query');
+define('TG_CHOSEN_INLINE','chosen_inline_query');
+define('TG_CALLBACK','callback_query');
+define('TG_SHIPPING','shipping_query');
+define('TG_CHECKOUT','pre_checkout_query');
+define('TG_POLL','poll');
+define('TG_POLL_ANSWER','poll_answer');
+define('TG_MY_CHAT','my_chat_member');
+define('TG_CHAT_MEMBER','chat_member');
+define('TG_JOIN','chat_join_request');
+
+define('TG_COUNT','photo_count');
+define('TG_NEW_COUNT','member_count');
+
+define('TG_SUBSCRIBED', 'member_subscribed');
+?>
