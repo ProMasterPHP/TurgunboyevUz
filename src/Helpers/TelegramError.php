296fe09552b866b99dafd8650577b3cdd7101ab4
ProMasterPHP <67572561+ProMasterPHP@users.noreply.github.com>
2023-03-09 20:20:17 +0500

    Add files via upload

---
+++ src/Helpers/TelegramError.php
@@ -0,0 +1,35 @@
+<?php
+namespace TurgunboyevUz;
+
+class TelegramError{
+	static function mysql($array){
+
+		$backtrace = debug_backtrace();
+
+		if($array['error_type'] == 'mysql_include'){
+			echo "<b>Warning: </b>".$array['description']." in ".$backtrace[1]['file']." on line ".$backtrace[1]['line'];
+		}
+
+		if(isset($array['error_code'])){
+			echo "<b>Error: </b>".$array['description']." in ".$backtrace[1]['file']." on line ".$backtrace[1]['line'];
+		}
+	}
+
+	static function useTraits($array){
+
+		$backtrace = debug_backtrace();
+
+		if($array['error_type'] == "use_trait"){
+			echo "<b>Warning: </b>".$array['description']." in ".$backtrace[1]['file']." on line ".$backtrace[1]['line'];
+		}
+	}
+
+	static function errorDashboard($array){
+		$backtrace = debug_backtrace();
+
+		if($array['error_type'] == "create_table"){
+			echo "<b>Warning: </b>".$array['description']." in ".$backtrace[1]['file']." on line ".$backtrace[1]['line'];
+		}	
+	}
+}
+?>
